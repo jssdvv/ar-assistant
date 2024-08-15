37b9a9ee4d8e39f338676ca02337894eec1a9eb0
jssdvv <jssdvv@gmail.com>
2024-08-15 00:39:46 -0500

    Add navigation bar

--- app/src/main/kotlin/com/jssdvv/ar_maintassist/core/presentation/navigation/components/NavBar.kt
+++ app/src/main/kotlin/com/jssdvv/ar_maintassist/core/presentation/navigation/components/NavBar.kt
@@ -1,4 +1,4 @@
-package com.jssdvv.ar_maintassist.core.presentation.navigation
+package com.jssdvv.ar_maintassist.core.presentation.navigation.components
 
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -9,60 +9,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.jssdvv.ar_maintassist.R
-
-sealed class NavItems(
-    val title: Int,
-    val route: Any,
-    val selectedIcon: Int,
-    val unselectedIcon: Int,
-    val description: String
-) {
-    data object HomeItem : NavItems(
-        title = R.string.home_title,
-        route = HomeGraph,
-        selectedIcon = R.drawable.home_filled,
-        unselectedIcon = R.drawable.home_outlined,
-        description = ""
-    )
-
-    data object CamItem : NavItems(
-        title = R.string.camera_title,
-        route = CameraGraph,
-        selectedIcon = R.drawable.ar_filled,
-        unselectedIcon = R.drawable.ar_outlined,
-        description = ""
-    )
-
-    data object DocsItem : NavItems(
-        title = R.string.docs_title,
-        route = DocumentationGraph,
-        selectedIcon = R.drawable.docs_filled,
-        unselectedIcon = R.drawable.docs_outlined,
-        description = ""
-    )
-
-    data object CalendarItem : NavItems(
-        title = R.string.calendar_title,
-        route = CalendarGraph,
-        selectedIcon = R.drawable.calendar_filled,
-        unselectedIcon = R.drawable.calendar_outlined,
-        description = ""
-    )
-}
 
 @Composable
 fun NavBar(
-    navController: NavHostController
+    navHostController: NavHostController,
+    modifier: Modifier = Modifier
 ) {
     var selectedItem by remember { mutableIntStateOf(0) }
-    val backStackEntry by navController.currentBackStackEntryAsState()
-    val currentScreen = backStackEntry?.destination?.parent?.route
+    val backStackEntry by navHostController.currentBackStackEntryAsState()
+
     NavigationBar {
         val navItems = listOf(
             NavItems.HomeItem,
@@ -74,8 +35,8 @@ fun NavBar(
             NavigationBarItem(
                 selected = true,
                 onClick = {
-                    navController.navigate(item.route) {
-                        navController.graph.findStartDestination().route?.let {
+                    navHostController.navigate(item.route) {
+                        navHostController.graph.findStartDestination().route?.let {
                             popUpTo(it) {
                                 inclusive = true
                             }
---
+++ app/src/main/kotlin/com/jssdvv/ar_maintassist/core/presentation/navigation/components/NavItems.kt
@@ -0,0 +1,47 @@
+package com.jssdvv.ar_maintassist.core.presentation.navigation.components
+
+import com.jssdvv.ar_maintassist.R
+import com.jssdvv.ar_maintassist.core.presentation.navigation.graphs.AgendaNavGraph
+import com.jssdvv.ar_maintassist.core.presentation.navigation.graphs.CameraNavGraph
+import com.jssdvv.ar_maintassist.core.presentation.navigation.graphs.HomeNavGraph
+import com.jssdvv.ar_maintassist.core.presentation.navigation.graphs.ResourcesNavGraph
+
+sealed class NavItems(
+    val title: Int,
+    val route: Any,
+    val selectedIcon: Int,
+    val unselectedIcon: Int,
+    val description: String
+) {
+    data object HomeItem : NavItems(
+        title = R.string.home_title,
+        route = HomeNavGraph,
+        selectedIcon = R.drawable.home_filled,
+        unselectedIcon = R.drawable.home_outlined,
+        description = ""
+    )
+
+    data object CamItem : NavItems(
+        title = R.string.camera_title,
+        route = CameraNavGraph,
+        selectedIcon = R.drawable.ar_filled,
+        unselectedIcon = R.drawable.ar_outlined,
+        description = ""
+    )
+
+    data object DocsItem : NavItems(
+        title = R.string.resources_title,
+        route = ResourcesNavGraph,
+        selectedIcon = R.drawable.docs_filled,
+        unselectedIcon = R.drawable.docs_outlined,
+        description = ""
+    )
+
+    data object CalendarItem : NavItems(
+        title = R.string.agenda_title,
+        route = AgendaNavGraph,
+        selectedIcon = R.drawable.calendar_filled,
+        unselectedIcon = R.drawable.calendar_outlined,
+        description = ""
+    )
+}
