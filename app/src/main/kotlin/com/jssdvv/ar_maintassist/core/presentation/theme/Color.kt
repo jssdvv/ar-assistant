629a1f8155ddef0976dc6639b51fd54da5a948f8
jssdvv <jssdvv@gmail.com>
2024-08-28 23:41:34 -0500

    Add color schemes

--- app/src/main/kotlin/com/jssdvv/ar_maintassist/core/presentation/theme/Color.kt
+++ app/src/main/kotlin/com/jssdvv/ar_maintassist/core/presentation/theme/Color.kt
@@ -2,10 +2,85 @@ package com.jssdvv.ar_maintassist.core.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Primary Key Colors
+val Primary0 = Color.Black
+val Primary10 = Color(0xFFFA8938)
+val Primary20 = Color(0xFFFA8938)
+val Primary30 = Color(0xFFFA8938)
+val Primary40 = Color(0XFFFA8938)
+val Primary50 = Color(0xFFFA8938)
+val Primary60 = Color(0xFFFA8938)
+val Primary70 = Color(0xFFfba66a)
+val Primary80 = Color(0XFFFCC49C)
+val Primary90 = Color(0XFFFEE1CD)
+val Primary100 = Color.White
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Secondary Key Colors
+val Secondary0 = Color.Black
+val Secondary10 = Color(0XFF2C2207)
+val Secondary20 = Color(0XFF58440E)
+val Secondary30 = Color(0XFF846615)
+val Secondary40 = Color(0XFFAF881D)
+val Secondary50 = Color(0XFFDBAA24)
+val Secondary60 = Color(0XFFE2BB50)
+val Secondary70 = Color(0XFFEACC7B)
+val Secondary80 = Color(0XFFF1DDA7)
+val Secondary90 = Color(0XFFF8EED3)
+val Secondary100 = Color.White
+
+// Tertiary Key Colors
+val Tertiary0 = Color.Black
+val Tertiary10 = Color(0XFF0E1A25)
+val Tertiary20 = Color(0XFF1C334A)
+val Tertiary30 = Color(0XFF2A4D6F)
+val Tertiary40 = Color(0XFF386694)
+val Tertiary50 = Color(0XFF4680B9)
+val Tertiary60 = Color(0XFF6B99C7)
+val Tertiary70 = Color(0XFF90B3D5)
+val Tertiary80 = Color(0XFFB5CCE3)
+val Tertiary90 = Color(0XFFDAE6F1)
+val Tertiary100 = Color.White
+
+// Neutral Key Colors
+val Neutral0 = Color.Black
+val Neutral5 = Color(0XFF0D0D0D)
+val Neutral10 = Color(0xFF1A1A1A)
+val Neutral15 = Color(0xFF262626)
+val Neutral20 = Color(0xFF333333)
+val Neutral25 = Color(0xFF404040)
+val Neutral30 = Color(0xFF4D4D4D)
+val Neutral40 = Color(0xFF666666)
+val Neutral50 = Color(0xFF808080)
+val Neutral60 = Color(0xFF999999)
+val Neutral70 = Color(0xFFB3B3B3)
+val Neutral80 = Color(0xFFCCCCCC)
+val Neutral85 = Color(0XFFD9D9D9)
+val Neutral90 = Color(0xFFE6E6E6)
+val Neutral95 = Color(0XFFF2F2F2)
+val Neutral100 = Color.White
+
+// Neutral Variant Key Colors
+val NeutralVariant0 = Color.Black
+val NeutralVariant10 = Color(0XFF1F1F14)
+val NeutralVariant20 = Color(0XFF3D3D29)
+val NeutralVariant30 = Color(0XFF5C5C3D)
+val NeutralVariant40 = Color(0XFF7A7A52)
+val NeutralVariant50 = Color(0XFF999966)
+val NeutralVariant60 = Color(0XFFADAD85)
+val NeutralVariant70 = Color(0XFFC2C2A3)
+val NeutralVariant80 = Color(0XFFD6D6C2)
+val NeutralVariant90 = Color(0XFFEBEBE0)
+val NeutralVariant100 = Color.White
+
+//Error Key Colors
+val Error0 = Color.Black
+val Error10 = Color(0xFF330500)
+val Error20 = Color(0XFF660A00)
+val Error30 = Color(0XFF990F00)
+val Error40 = Color(0XFFCC1400)
+val Error50 = Color(0XFFFF1A00)
+val Error60 = Color(0XFFFF4733)
+val Error70 = Color(0XFFFF7566)
+val Error80 = Color(0XFFFFA399)
+val Error90 = Color(0XFFFFD1CC)
+val Error100 = Color.White
