c5cfa68c9499f21bc0ac38137dd9cd872539abc9
jssdvv <jssdvv@gmail.com>
2024-09-06 20:18:21 -0500

    Fix MachineTypeConverter

--- app/src/main/kotlin/com/jssdvv/ar_maintassist/machines/data/local/MachineDatabase.kt
+++ app/src/main/kotlin/com/jssdvv/ar_maintassist/machines/data/local/MachineDatabase.kt
@@ -2,9 +2,12 @@ package com.jssdvv.ar_maintassist.machines.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.jssdvv.ar_maintassist.machines.domain.models.MachineEntity
+import com.jssdvv.ar_maintassist.machines.domain.models.MachineTypeConverter
 
 @Database(entities = [MachineEntity::class], version = 1, exportSchema = false)
+@TypeConverters(MachineTypeConverter::class)
 abstract class MachineDatabase : RoomDatabase() {
     abstract val dao: MachineDao
 
--- app/src/main/kotlin/com/jssdvv/ar_maintassist/machines/domain/models/MachineEntity.kt
+++ app/src/main/kotlin/com/jssdvv/ar_maintassist/machines/domain/models/MachineEntity.kt
@@ -1,8 +1,10 @@
 package com.jssdvv.ar_maintassist.machines.domain.models
 
+import android.net.Uri
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
 
 @Entity(tableName = "machineTable")
 data class MachineEntity(
@@ -10,8 +12,21 @@ data class MachineEntity(
     @ColumnInfo(name = "id") val id: Int = 0,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "category") val category: String,
+    @ColumnInfo(name = "imageUri") val imageUri: Uri?,
     @ColumnInfo(name = "description") val description: String,
     @ColumnInfo(name = "timestamp") val timestamp: Long
 )
 
 class InvalidMachineException(exception: String) : Exception(exception)
+
+class MachineTypeConverter {
+    @TypeConverter
+    fun uriToString(uri: Uri?): String? {
+        return uri?.toString()
+    }
+
+    @TypeConverter
+    fun stringToUri(string: String?): Uri? {
+        return if (string != null) Uri.parse(string) else null
+    }
+}
