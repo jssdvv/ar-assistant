a46c613fd1229d1484e3ad58b7e0dd3e4c9b71b3
jssdvv <jssdvv@gmail.com>
2024-09-06 20:07:53 -0500

    Add default names for MachineOrderKey.kt

--- app/src/main/kotlin/com/jssdvv/ar_maintassist/machines/domain/utils/MachineOrderKey.kt
+++ app/src/main/kotlin/com/jssdvv/ar_maintassist/machines/domain/utils/MachineOrderKey.kt
@@ -3,9 +3,29 @@ package com.jssdvv.ar_maintassist.machines.domain.utils
 import com.jssdvv.ar_maintassist.core.domain.utils.OrderType
 
 sealed class MachineOrderKey(
-    val orderType: OrderType
+    val orderType: OrderType,
+    val orderKeyName: String
 ) {
-    class Name(orderType: OrderType) : MachineOrderKey(orderType)
-    class Category(orderType: OrderType) : MachineOrderKey(orderType)
-    class Timestamp(orderType: OrderType) : MachineOrderKey(orderType)
+    class Name(
+        orderType: OrderType,
+        orderKeyName: String = "Name"
+    ) : MachineOrderKey(orderType, orderKeyName)
+
+    class Category(
+        orderType: OrderType,
+        orderKeyName: String = "Category"
+    ) : MachineOrderKey(orderType, orderKeyName)
+
+    class Timestamp(
+        orderType: OrderType,
+        orderKeyName: String = "Creation Date"
+    ) : MachineOrderKey(orderType, orderKeyName)
+
+    fun copy(orderType: OrderType): MachineOrderKey {
+        return when (this) {
+            is Name -> Name(orderType)
+            is Category -> Category(orderType)
+            is Timestamp -> Timestamp(orderType)
+        }
+    }
 }
