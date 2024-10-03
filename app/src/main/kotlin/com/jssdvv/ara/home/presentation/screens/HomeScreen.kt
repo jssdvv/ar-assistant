790080ebf0d418ebfbec9666f40edab7273a6ccc
jssdvv <jssdvv@gmail.com>
2024-10-03 01:25:07 -0500

    Create base files for new feature 'scanner'

--- app/src/main/kotlin/com/jssdvv/ara/home/presentation/screens/HomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.jssdvv.ara.home.presentation.screens
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-
-@Composable
-fun HomeScreen(
-    navHostController: NavHostController
-) { }
